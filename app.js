0b2c24acf8676ac32ec7f84aee493037e941bd5f
Sanjeeban2411 <sanju.sanjeeban.sp@gmail.com>
2023-03-12 21:59:19 +0530

    GET request made to get data from site

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require("fs/promises");
 const express = require("express");
 const app = express();
 
-// app.get("/getdetails/:dest", async function (req, res) {
+app.get("/getdetails/:dest", async function (req, res) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   page.setDefaultNavigationTimeout(0);
@@ -18,22 +18,23 @@ const app = express();
     page.click("#search-results article div a"),
     page.waitForNavigation({ waitUntil: "load" }),
   ]);
-  let data = await page.evaluate(
-    () =>
-      document
-        .querySelector("*")
-        .outerHTML.split('<script id="__NEXT_DATA__" type="application/json">')
-        .pop()
-        .split(',"scriptLoader":[]}</script>')[0] + "}"
-  );
+  // let data = await page.evaluate(
+  //   () =>
+  //     document
+  //       .querySelector("*")
+  //       .outerHTML.split('<script id="__NEXT_DATA__" type="application/json">')
+  //       .pop()
+  //       .split(',"scriptLoader":[]}</script>')[0] + "}"
+  // );
+
   data = JSON.parse(data);
   // console.log(typeof data);
   await browser.close().then(async () => {
     console.log("browser closed");
-    const final = {"data": data.props.pageProps.data.points}
+    const final = { data: data.props.pageProps.data.points };
     res.send(final);
   });
-// });
+});
 
 app.listen(8000, function () {
   console.log("Running on port 8000.");
